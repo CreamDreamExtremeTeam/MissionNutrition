89401bfb5c50def66f60113159ad08e5a5f072f7
William Lan <wlan76@yahoo.com>
2018-12-04 11:24:52 -0800

    Add some new code

--- assets/Database.js
+++ assets/Database.js
@@ -54,8 +54,10 @@ database.ref().on("child_added", function (snapshot) {
    nutritionElem.text(sv.nutrition);
    limitElem.text(sv.limit);
 
+   nutritionElem.append(nutritionElem);
+   limitElem.append(limitElem);
 
-
+   // Handle the errors
 }, function (errorObject) {
    console.log("Errors handled: :" + errorObject.code);
 });
