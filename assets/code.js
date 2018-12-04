487dd81f2e21840b3f419c9026ab48c77bed2baf
Cole Johnson <coleharrisjohnson@gmail.com>
2018-12-04 11:46:12 -0800

    removed comment

--- assets/code.js
+++ assets/code.js
@@ -1,14 +1,3 @@
-
-// var fbConfig = {
-//    apiKey: "AIzaSyB8FG59wi_LDzcg8PDFBc_X-7A7e82TjHY",
-//    authDomain: "mission-nutrition-71ab9.firebaseapp.com",
-//    databaseURL: "https://mission-nutrition-71ab9.firebaseio.com",
-//    projectId: "mission-nutrition-71ab9",
-//    storageBucket: "mission-nutrition-71ab9.appspot.com",
-//    messagingSenderId: "876456885465"
-// };
-// firebase.initializeApp(fbConfig);
-
 $(document).ready(main);
 var foodArray = [];
 
