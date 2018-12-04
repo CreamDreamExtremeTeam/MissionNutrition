f34e18dfde78c0ffa7494295cdc45168b5d57d5c
Shuma Gorath <jmgordon1988@gmail.com>
2018-12-04 09:54:11 -0800

    giphy functionality added to code.js

--- assets/Database.js
+++ assets/Database.js
@@ -1,17 +1,17 @@
 var fbConfig = {
-   apiKey: "AIzaSyB8FG59wi_LDzcg8PDFBc_X-7A7e82TjHY",
-   authDomain: "mission-nutrition-71ab9.firebaseapp.com",
-   databaseURL: "https://mission-nutrition-71ab9.firebaseio.com",
-   projectId: "mission-nutrition-71ab9",
-   storageBucket: "mission-nutrition-71ab9.appspot.com",
-   messagingSenderId: "876456885465"
-};
-firebase.initializeApp(fbConfig);
-
-//create variable to reference the database
-var databse = firebase.databse();
-
-var nutrition = "";
-var limit = "";
-
-
+    apiKey: "AIzaSyB8FG59wi_LDzcg8PDFBc_X-7A7e82TjHY",
+    authDomain: "mission-nutrition-71ab9.firebaseapp.com",
+    databaseURL: "https://mission-nutrition-71ab9.firebaseio.com",
+    projectId: "mission-nutrition-71ab9",
+    storageBucket: "mission-nutrition-71ab9.appspot.com",
+    messagingSenderId: "876456885465"
+ };
+ firebase.initializeApp(fbConfig);
+ 
+ //create variable to reference the database
+ var database = firebase.database();
+ 
+ var nutrition = "";
+ var limit = "";
+ 
+ 
